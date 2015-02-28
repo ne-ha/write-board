9e59ac18abdff839f450d26197f09248f1d57bb1
NEHA SUWAl <neha@jyaasa.com>
2015-02-28 14:52:25 +0545

    created table notes and ran rake db migrate

--- db/migrate/20150228090007_create_notes.rb
+++ db/migrate/20150228090007_create_notes.rb
@@ -1,6 +1,9 @@
 class CreateNotes < ActiveRecord::Migration
   def change
     create_table :notes do |t|
+      t.integer :user_id
+      t.string :title
+      t.text :body
 
       t.timestamps null: false
     end
