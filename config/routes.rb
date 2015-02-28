59fcf867af9e0207cff10e0f8f1501ccf3a157b0
NEHA SUWAl <neha@jyaasa.com>
2015-02-28 15:14:56 +0545

    modified the root path from users index to notes index

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root to: "users#index"
+  root to: "notes#index"
 
   resources :users
 
