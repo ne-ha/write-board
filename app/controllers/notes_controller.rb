2e06d6a9d40b0b33c56caf5569680f1f9e9bc861
NEHA SUWAl <neha@jyaasa.com>
2015-02-28 15:09:12 +0545

    written code for index , show and create notes. And defined a private method notes_params

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,33 @@
 class NotesController < ApplicationController
-  def show
+
+  before_action :authenticate_user!
+
+  def index
+    @notes = current_user.notes
   end
+
+  def show
+    @note = current_user.notes.where(:id=> params[:id])
+  end
+
+  def create
+    @note = current_user.notes.create(notes_params)
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def destroy
+  end
+
+  private
+    def notes_params
+      params.require(:note).permit(:title, :body)
+    end
 end
