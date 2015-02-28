5e26e875e852cde6ff5bd8dd4a5f5de5bfa681e2
NEHA SUWAl <neha@jyaasa.com>
2015-02-28 14:49:23 +0545

    generated controller Notes

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,19 @@
+class NotesController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def destroy
+  end
+end
