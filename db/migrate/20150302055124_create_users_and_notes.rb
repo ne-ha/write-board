8f953196b3e2079b51f40f5d52beae979d08e4c7
NEHA SUWAl <neha@jyaasa.com>
2015-03-02 12:27:12 +0545

    deleted this file

--- db/migrate/20150302055124_create_users_and_notes.rb
+++
@@ -1,8 +0,0 @@
-class CreateUsersAndNotes < ActiveRecord::Migration
-  def change
-    create_table :users_and_notes, id: false do |t|
-      t.belongs_to :user, index: true
-      t.belongs_to :note, index: true
-    end
-  end
-end
