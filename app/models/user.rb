1b3154ee4a5eccd3b8653472e6361f649e846678
NEHA SUWAl <neha@jyaasa.com>
2015-03-02 17:04:48 +0545

    modified association between user and note

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_and_belongs_to_many :notes
+  has_many :notes
   
 end
