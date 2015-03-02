1b4d4978928a47fa7902f39be2a8e1b150bd39e8
NEHA SUWAl <neha@jyaasa.com>
2015-03-02 22:42:04 +0545

    added two columns first name and lastname to users table and ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150302161503) do
+ActiveRecord::Schema.define(version: 20150302165603) do
 
   create_table "notes", force: :cascade do |t|
     t.integer  "user_id"
@@ -50,6 +50,8 @@ ActiveRecord::Schema.define(version: 20150302161503) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
