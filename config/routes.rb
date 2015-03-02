13eaea8d176a2c937e03a9d5f4a314a8e284a437
NEHA SUWAl <neha@jyaasa.com>
2015-03-02 14:06:50 +0545

    added new actin in notes controller

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,9 @@ Rails.application.routes.draw do
 
   resources :users
 
-  resources :notes
-
+  resources :notes do
+    get 'share_note'
+  end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
