1461dcb2d1df74d6fd9c286c65c6277bec89e03e
NEHA SUWAl <neha@jyaasa.com>
2015-03-02 17:21:36 +0545

    removed js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,10 +15,3 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
-
-$(document).ready(function(){
-  $(".share_to").click(function(){
-    $("#shareModal").modal("show");
-  });
-});
-
