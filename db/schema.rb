72f6924fe314a1ad4d4638f1b06896d0c4eabb45
NEHA SUWAl <neha@jyaasa.com>
2015-03-02 13:47:01 +0545

    ran rake db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150302063744) do
+ActiveRecord::Schema.define(version: 20150302075725) do
 
   create_table "notes", force: :cascade do |t|
     t.integer  "user_id"
@@ -42,7 +42,6 @@ ActiveRecord::Schema.define(version: 20150302063744) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "owner"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
