e7746d312a7261f3d8428dc0ecc8088a4c76f4ff
NEHA SUWAl <neha@jyaasa.com>
2015-03-02 11:38:34 +0545

    created new migration for users and notes

---
+++ db/migrate/20150302055124_create_users_and_notes.rb
@@ -0,0 +1,8 @@
+class CreateUsersAndNotes < ActiveRecord::Migration
+  def change
+    create_table :users_and_notes, id: false do |t|
+      t.belongs_to :user, index: true
+      t.belongs_to :note, index: true
+    end
+  end
+end
