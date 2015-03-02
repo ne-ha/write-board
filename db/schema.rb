0b5a378eb53211ad455ebe1e44f040f951acada2
NEHA SUWAl <neha@jyaasa.com>
2015-03-02 11:33:00 +0545

    after adding new column to users table ran migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150228090007) do
+ActiveRecord::Schema.define(version: 20150302054653) do
 
   create_table "notes", force: :cascade do |t|
     t.integer  "user_id"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20150228090007) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "owner"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
