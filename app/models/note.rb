d36a1fd8f312a453f01b4ebaf6d476e21f907ea9
NEHA SUWAl <neha@jyaasa.com>
2015-03-03 14:49:51 +0545

    maintained relation note belongs to one owner

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,7 +1,7 @@
 class Note < ActiveRecord::Base
 
-  belongs_to :user
-
+  has_and_belongs_to_many :users
+  belongs_to :owner, class_name: "User"
   validates :title , :presence => true
 
   serialize :share_with
