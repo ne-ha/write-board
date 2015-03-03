6ae3317e0425875c5190a9dc9dd0ac0a63f74ded
NEHA SUWAl <neha@jyaasa.com>
2015-03-03 17:16:05 +0545

    required best_in place

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery.purr
 //= require best_in_place
+//= require best_in_place.purr
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
