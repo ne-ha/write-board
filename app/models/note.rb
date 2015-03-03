cad8493105204fe4018ded7db3aa156cac57a25b
NEHA SUWAl <neha@jyaasa.com>
2015-03-03 15:20:35 +0545

    added foreign key

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,7 +1,7 @@
 class Note < ActiveRecord::Base
 
   has_and_belongs_to_many :users
-  belongs_to :owner, class_name: "User"
+  belongs_to :owner, class_name: "User", foreign_key: :owner_id
   validates :title , :presence => true
 
   serialize :share_with
