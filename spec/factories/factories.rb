b3e66aa6e6ea02e561d3eccda3dfeef8f162276c
NEHA SUWAl <neha@jyaasa.com>
2015-03-04 13:59:28 +0545

    sequence is changed to normal email

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,10 +1,8 @@
 FactoryGirl.define do 
-  factory :user do |f| 
-    f.sequence :email do |e|
-      "user#{e}@example.com"
-    end
-    f.password "secretpassword"
-    f.password_confirmation "secretpassword"
+  factory :user do 
+    email "test@example.com"
+    password "secretpassword"
+    password_confirmation "secretpassword"
   end
 
   factory :note do
