8c988fc39c1cfef063cb6719fef32687f3279637
NEHA SUWAl <neha@jyaasa.com>
2015-03-04 09:25:59 +0545

    required rails root and factory girl

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,11 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
-require 'rspec/rails'
-require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
+require 'rspec/rails'
+require 'factory_girl-rails'
+require "#{Rails.root}/spec/factories.rb"
+require 'spec_helper'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
