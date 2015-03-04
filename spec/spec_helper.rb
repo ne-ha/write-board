ab86eb9f13361b85960c2f0a2d8f40469e769570
NEHA SUWAl <neha@jyaasa.com>
2015-03-04 11:44:53 +0545

    required database clearner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,7 @@
 #
 # The `.rspec` file also contains a few flags that are not defaults but that
 # users commonly want.
-
+require 'database_cleaner'
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
