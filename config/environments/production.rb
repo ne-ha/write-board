431c23e18c81a03eb6e758b58a60cb5dbd28e042
NEHA SUWAl <neha@jyaasa.com>
2015-03-05 18:55:44 +0545

    added configuration for devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,12 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.log_level = :warn
+
+  config.action_mailer.default_url_options = {:host => 'testtalkruby@gmail.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "127.0.0.1",
+    :port    => 25,
+    :domain  => 'testtalkruby@gmail.com'
+  }
 end
