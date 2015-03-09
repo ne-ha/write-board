91d5f52837c1d5b089ab5f57950016c1d04ad96e
NEHA SUWAl <neha@jyaasa.com>
2015-03-09 09:45:11 +0545

    added method password_required and password_match for setting password after confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,15 @@ class User < ActiveRecord::Base
 
   has_and_belongs_to_many :notes
 
+  def password_required?
+    super if confirmed?
+  end
+
+  def password_match?
+    self.errors[:password] << "can't be blank" if password.blank?
+    self.errors[:password_confirmation] << "can't be blank" if password_confirmation.blank?
+    self.errors[:password_confirmation] << "does not match password" if password != password_confirmation
+    password == password_confirmation && !password.blank?
+  end
+
 end
